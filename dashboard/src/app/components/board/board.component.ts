0254546db4aec58258f3879a7c18036f1c679c10
Anastasiia <troyanovskaya2003@gmail.com>
2022-11-05 22:50:06 +0000

    feat: sort buttons work
    refactor: bord component generates content dynamicly via db.service boards array and passed index

--- dashboard/src/app/components/board/board.component.ts
+++ dashboard/src/app/components/board/board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DbAccessService } from 'src/app/services/db-access.service';
 import { Board } from '../../models/board.model'
 
 
@@ -9,10 +10,14 @@ import { Board } from '../../models/board.model'
 })
 export class BoardComponent implements OnInit {
   @Input() board:Board
+  @Input() i:any
   description = false;
   descriptionText='Show description';
+  constructor(public db:DbAccessService){
+  }
 
   changeButtonState(){
+
     this.description = !this.description;
     if(this.descriptionText === 'Show description'){
       this.descriptionText = 'Hide description';
--- dashboard/src/app/components/line-of-buttons/line-of-buttons.component.ts
+++ dashboard/src/app/components/line-of-buttons/line-of-buttons.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Board } from 'src/app/models/board.model';
+import { DbAccessService } from 'src/app/services/db-access.service';
 import { KeywordService } from 'src/app/services/keyword.service';
 
 @Component({
@@ -10,9 +11,22 @@ import { KeywordService } from 'src/app/services/keyword.service';
 export class LineOfButtonsComponent {
   buttonNames = ['Show oldest projects', 'Show recent projects'];
   inputName = '';
-  constructor(private keyService:KeywordService) { }
+  constructor(private keyService:KeywordService, public db:DbAccessService) { }
   sortASC(){
+    this.db.boards.sort((el1, el2)=>{
+      let a = el1.date.split('-').reverse().join('');
+      let b = el2.date.split('-').reverse().join('');
+      return a.localeCompare(b);
+    })
 
+  }
+
+  sortDSC(){
+    this.db.boards.sort((el1, el2)=>{
+      let a = el1.date.split('-').reverse().join('');
+      let b = el2.date.split('-').reverse().join('');
+      return b.localeCompare(a);
+    })
   }
   emit(){
     this.keyService.key = this.inputName;
