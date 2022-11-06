3a69405eacbe716c0201381a97f81d0b8817c7ca
Anastasiia <troyanovskaya2003@gmail.com>
2022-11-06 01:01:59 +0000

    feat: draft of put request, works statically

--- dashboard/src/app/services/db-access.service.ts
+++ dashboard/src/app/services/db-access.service.ts
@@ -6,7 +6,7 @@ import { Board } from '../models/board.model'
   providedIn: 'root'
 })
 export class DbAccessService{
-  url: string = 'http://localhost:3000/Boards  '
+  url: string = 'http://localhost:3000/Boards'
   boards:Board[] = [];
   constructor(private http: HttpClient) {
     this.getBoards().subscribe(res => {this.boards=res},
@@ -22,5 +22,9 @@ export class DbAccessService{
     this.getBoards().subscribe(res => {this.boards=res},
       err => console.log('Error Occured ' + err));
   }
-
+  addNote(id:number, board:Board){
+    let url=this.url+'/'+id;
+    this.http.put(url, board).subscribe(data => console.log(data));;
+    console.log(url);
+  }
 }
