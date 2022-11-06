330bc620919de27f51f7a7671c7307aa2e65ae61
Anastasiia <troyanovskaya2003@gmail.com>
2022-11-06 19:34:37 +0000

    feat: add in progress and done notes to list, add option hide textarea on all lists

--- dashboard/src/app/components/full-board-information/full-board-information.component.ts
+++ dashboard/src/app/components/full-board-information/full-board-information.component.ts
@@ -10,11 +10,20 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./full-board-information.component.css']
 })
 export class FullBoardInformationComponent implements OnInit {
+  isVisibleForm1:boolean = false;
+  isVisibleForm2:boolean = false;
+  isVisibleForm3:boolean = false;
   id:number = 0;
   board:Board;
   form1: FormGroup = new FormGroup({noteToDo: new FormControl<string>('', [
     Validators.required
-  ])})
+  ])});
+  form2: FormGroup = new FormGroup({noteProgress: new FormControl<string>('', [
+    Validators.required
+  ])});
+  form3: FormGroup = new FormGroup({noteDone: new FormControl<string>('', [
+    Validators.required
+  ])});
   showBoard(){
     console.log(this.board);
   }
@@ -23,6 +32,20 @@ export class FullBoardInformationComponent implements OnInit {
       this.board.todo.push(this.form1.controls.noteToDo.value.slice(0, -1));
       this.db.addNote(this.id, this.board);
       this.form1.reset();
+    }
+  }
+  sendProgress(){
+    if(this.form2.controls.noteProgress.value.trim().length !== 0){
+      this.board.progress.push(this.form2.controls.noteProgress.value.slice(0, -1));
+      this.db.addNote(this.id, this.board);
+      this.form2.reset();
+    }
+  }
+  sendDone(){
+    if(this.form3.controls.noteDone.value.trim().length !== 0){
+      this.board.done.push(this.form3.controls.noteDone.value.slice(0, -1));
+      this.db.addNote(this.id, this.board);
+      this.form3.reset();
     }
   }
   constructor(private route: ActivatedRoute, public db:DbAccessService) { }
